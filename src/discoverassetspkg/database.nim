699f0cd4ec79f429b93ce0926d2cdb3fdc5ba6d2
C-NERD <alayaa694@gmail.com>
2021-05-06 04:02:18 +0100

    updated html template

--- src/discoverassetspkg/database.nim
+++ src/discoverassetspkg/database.nim
@@ -1,4 +1,5 @@
 import db_sqlite
+from datafuncs import addApi, Api
 
 var db  = open("database.db", "", "", "")
 
@@ -9,20 +10,19 @@ proc newdatabase() =
     db.exec(sql"""
     CREATE TABLE IF NOT EXISTS site(
         id int NOT NULL,
-        logo blob
+        logo blob,
+        PRIMARY KEY (id)
     );
     """)
 
     db.exec(sql"""
     CREATE TABLE IF NOT EXISTS api(
-        apiid int NOT NULL,
         siteid int NOT NULL REFERENCES site,
         link text,
-        link_tag text,
-        icon text,
-        icon_tag text,
         dimension text,
         website text,
+        icon text,
+        icon_tag text,
         asset_class text,
         asset_tag text,
         name_class text,
@@ -30,12 +30,56 @@ proc newdatabase() =
         img_class text,
         img_tag text,
         assetlink_class text,
-        assetlink_tag text,
-        PRIMARY KEY (apiid)
+        assetlink_tag text
     );
     """)
 
     discard db.insertID(sql"INSERT INTO site (id) VALUES (?)", $1)
 
+
+proc populate() =
+    const sites : seq[Api]  =
+        @[
+            Api(
+                link : "https://3dexport.com/free-search/keywords($1)?page=$2",
+                dimension : "3D",
+                website : "3dexport.com"
+            ),
+            Api(
+                link : "https://clara.io/library?gameCheck=true&public=true&query=$1&page=$2&perPage=$3",
+                dimension : "3D",
+                website : "clara.io"
+            ),
+            Api(
+                link : "https://www.blendswap.com/search?page=$2&keyword=$1",
+                dimension : "3D",
+                website : "blendswap.com"
+            ),
+            Api(
+                link : "https://3dmodelhaven.com/models/?c=&s=$1",
+                dimension : "3D",
+                website : "3dmodelhaven"
+            ),
+            Api(
+                link : "https://opengameart.org/art-search?keys=$1&page=$2",
+                dimension : "2D",
+                website : "opengameart.org"
+            ),
+            Api(
+                link : "https://www.gameart2d.com/freebies.html",
+                dimension : "2D",
+                website : "gameart2d.com"
+            ),
+            Api(
+                link : "https://craftpix.net/page/$2/?s=$1",
+                dimension : "2D",
+                website : "craftpix.net"
+            )
+        ]
+
+    for site in sites:
+        db.addApi(site)
+
 newdatabase()
+populate()
 db.close()
--- src/discoverassetspkg/datafuncs.nim
+++ src/discoverassetspkg/datafuncs.nim
@@ -1,7 +1,5 @@
 import db_sqlite
 
-var db = open("database.db", "", "", "")
-
 type
     Data* = object   
         img* : string
@@ -12,11 +10,10 @@ type
 
     Api* = object
         link* : string
-        link_tag* : string
-        icon* : string
-        icon_tag* : string
         dimension* : string
         website* : string
+        icon* : string
+        icon_tag* : string
         asset_class* : string
         asset_tag* : string
         name_class* : string
@@ -31,38 +28,37 @@ type
         apis* : seq[Api]
 
 
-proc updateLogo*(logo : string) =
+proc updateLogo*(db : DbConn, logo : string) =
     db.exec(sql"UPDATE site SET logo = ? WHERE id = ?;", logo, $1)
 
 
-proc addApi*(api : Api) =
+proc addApi*(db : DbConn, api : Api) =
     discard db.insertID(sql"""INSERT INTO api 
-    (siteid, link, link_tag, icon, icon_tag, dimension, website, asset_class, asset_tag,
+    (siteid, link, dimension, website, icon, icon_tag, asset_class, asset_tag,
     name_class, name_tag, img_class, img_tag, assetlink_class, assetlink_tag)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""", $1, api.link, api.link_tag,
-    api.icon, api.icon_tag, api.dimension, api.website, api.asset_class, api.asset_tag,
-    api.name_class, api.name_tag, api.img_class, api.img_tag, api.assetlink_class, api.assetlink_tag)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);""", $1, api.link, api.dimension, 
+    api.website, api.icon, api.icon_tag, api.asset_class, api.asset_tag, api.name_class, 
+    api.name_tag, api.img_class, api.img_tag, api.assetlink_class, api.assetlink_tag)
 
 
-proc getSearch*() : Site =
+proc getSearch*(db : DbConn) : Site =
     result.logo = db.getValue(sql"SELECT logo FROM site WHERE id = ?;", $1)
 
     for api in db.fastRows(sql"SELECT * FROM api WHERE siteid = 1;"):
         var info : Api
 
         info.link = api[2]
-        info.link_tag = api[3]
-        info.icon = api[4]
-        info.icon_tag = api[5]
-        info.dimension = api[6]
-        info.website = api[7]
-        info.asset_class = api[8]
-        info.asset_tag = api[9]
-        info.name_class = api[10]
-        info.name_tag = api[11]
-        info.img_class = api[12]
-        info.img_tag = api[13]
-        info.assetlink_class = api[14]
-        info.assetlink_tag = api[15]
+        info.dimension = api[3]
+        info.website = api[4]
+        info.icon = api[5]
+        info.icon_tag = api[6]
+        info.asset_class = api[7]
+        info.asset_tag = api[8]
+        info.name_class = api[9]
+        info.name_tag = api[10]
+        info.img_class = api[11]
+        info.img_tag = api[12]
+        info.assetlink_class = api[13]
+        info.assetlink_tag = api[14]
 
         result.apis.add(info)
